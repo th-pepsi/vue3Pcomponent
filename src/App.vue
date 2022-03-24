b8822e786393a898d660de91525127244bb90f5b
th-pepsi <963889833@qq.com>
2022-03-24 22:51:49 +0800

    'feat:新增'

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
   <div style="padding-left:20px">
-    <br>
+    <!-- <br>
     <Button></Button>
     <br>
     <Icon></Icon>
     <br>
-    <Modal></Modal>
+    <Modal></Modal> -->
+    <p-button  type="primary">哈哈哈</p-button>
   </div>
 </template>
 
 <script>
-import Button from "../examples/button";
-import Icon from "../examples/icon";
-import Modal from "../examples/modal";
+// import Button from "../examples/button";
+// import Icon from "../examples/icon";
+// import Modal from "../examples/modal";
 export default {
   name: "App",
   components: {
-    Button,
-    Icon,
-    Modal
+ 
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Peui from './packages'
+import Peui from 'pe-ui/src/packages'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import './style/iconfont/iconfont.css'
+
+console.log(Peui)
 
 createApp(App).use(Peui).use(Antd).mount('#app')
